3ec07e50c663f1fa691e14b068372ce089cd3c75
Martin Gentles <martingentles13@gmail.com>
2023-03-05 23:28:03 -0500

    final adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include <ArduinoJson.h>
 #include "env.h" // remember to add this file to the .gitignore
 
-#define light_switch_1 21
+#define light_switch_1 23
 #define light_switch_2 22
-#define light_switch_3 23
+#define light_switch_3 21
+
 
 bool blinkcounter[8][3] =
  {
@@ -22,16 +23,14 @@ bool blinkcounter[8][3] =
 
 
 //Your Domain/IP Address name with URL route
-const char * endpoint = "URI";
+const char * endpoint = URI;
 
 void setup() {
 
   pinMode(light_switch_1, OUTPUT);
   pinMode(light_switch_2, OUTPUT);
   pinMode(light_switch_2, OUTPUT);
-  digitalWrite(light_switch_1,HIGH);
-  digitalWrite(light_switch_2,LOW);
-  digitalWrite(light_switch_1,LOW);
+
   Serial.begin(9600);
 	
 	// WiFi_SSID and WIFI_PASS should be stored in the env.h
@@ -58,75 +57,75 @@ void loop() {
     http.begin(endpoint);
     
     // Specify content-type header
-    http.addHeader("X-API-Key", KEYVALUE);
-    http.addHeader("Content-type", "application-type");
-    http.addHeader("Content-length", "76");
-    //http.addHeader("Host", "https://ecse-three-led-api-v2.onrender.com");
-
-    // Serialise JSON object into a string to be sent to the API
-    StaticJsonDocument<64> doc;
-    char * httpRequestData;
+   
 
       for(int n = 0; n < 8; n++)
       {
-        for(int i = 0; i < 3; i++)
-        {
-          digitalWrite(light_switch_1, blinkcounter[n][i]);
-          digitalWrite(light_switch_2, blinkcounter[n][i]);
-          digitalWrite(light_switch_3, blinkcounter[n][i]);
+        http.addHeader("X-API-Key", KEYVALUE);
+        http.addHeader("Content-type", "application/json");
+          //http.addHeader("Host", "https://ecse-three-led-api-v2.onrender.com");
 
-          doc["light_switch_1"] = blinkcounter[n][i]; 
-          doc["light_switch_2"] = blinkcounter[n][i];
-          doc["light_switch_3"] = blinkcounter[n][i];
-        }
-      }
-     
+          // Serialise JSON object into a string to be sent to the API
+        StaticJsonDocument<72> doc;
+        String httpRequestData;
 
 
-    //doc["light_switch_1"] = digitalRead(light_switch_1);
-    //doc["light_switch_2"] =  digitalRead(light_switch_2);
-    //doc["light_switch_3"] =  digitalRead(light_switch_3);
-		
-		// convert JSON document, doc, to string and copies it into httpRequestData
-    serializeJson(doc, httpRequestData);
+        digitalWrite(light_switch_1, blinkcounter[n][0]);
+        delay(2000);
+        digitalWrite(light_switch_2, blinkcounter[n][1]);
+        delay(2000);
+        digitalWrite(light_switch_3, blinkcounter[n][2]);
+        delay(2000);
 
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(httpRequestData);
-    String http_response;
 
-    // check reuslt of POST request. negative response code means server wasn't reached
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+        doc["light_switch_1"] = blinkcounter[n][0]; 
+        doc["light_switch_2"] = blinkcounter[n][1];
+        doc["light_switch_3"] = blinkcounter[n][2];
 
-      Serial.print("HTTP Response from server: ");
-      http_response = http.getString();
-      Serial.println(http_response);
+        serializeJson(doc, httpRequestData);
 
-      StaticJsonDocument<1024> doc;
+         // Send HTTP POST request
+        int httpResponseCode = http.PUT(httpRequestData);
 
-      DeserializationError error = deserializeJson(doc, http_response);
+        String http_response;
+      // check reuslt of POST request. negative response code means server wasn't reached
+          if (httpResponseCode > 0) 
+          {
+            Serial.print("HTTP Response code: ");
+            Serial.println(httpResponseCode); 
 
-      if (error) {
-        Serial.print("deserializeJson() failed: ");
-        Serial.println(error.c_str());
-        return;
-      }
-    }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-   
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
+            Serial.print("HTTP Response from server: ");
+            http_response = http.getString();
+            Serial.println(http_response);
+
+            
+            StaticJsonDocument<1024> doc;
+
+            DeserializationError error = deserializeJson(doc, http_response);  
+       
+          if (error) {
+            Serial.print("deserializeJson() failed: ");
+            Serial.println(error.c_str());
+            return;
+          }
+          else
+          {
+            Serial.print("Error code: ");
+            Serial.println(httpResponseCode);
+          }
+
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+
+          http.end();
       
-    // Free resources
-    http.end();
-  }
-  else {
-    Serial.println("WiFi Disconnected");
-  }
+        }
+      }     
+   
+      }
+      else {
+      Serial.println("WiFi Disconnected");
+    }
 }
 
 
